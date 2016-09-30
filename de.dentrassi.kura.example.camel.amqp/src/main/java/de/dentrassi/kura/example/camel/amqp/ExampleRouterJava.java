cbf8ac9dfb7cfc75adea0a7e82389b6752ee2851
Jens Reimann <jreimann@redhat.com>
2016-09-30 12:22:49 +0200

    Refactor the example to support the new API, apply Apache License 2

--- de.dentrassi.kura.example.camel.amqp/src/main/java/de/dentrassi/kura/example/camel/amqp/ExampleRouterJava.java
+++ de.dentrassi.kura.example.camel.amqp/src/main/java/de/dentrassi/kura/example/camel/amqp/ExampleRouterJava.java
@@ -1,82 +1,170 @@
-/*******************************************************************************
- * Copyright (c) 2016 Red Hat Inc and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/*
+ * Copyright (C) 2016 Jens Reimann <jreimann@redhat.com>
  *
- * Contributors:
- *     Red Hat Inc - initial API and implementation
- *******************************************************************************/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.dentrassi.kura.example.camel.amqp;
+
+import static org.eclipse.kura.camel.component.Configuration.asString;
 
 import java.util.Map;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.amqp.AMQPComponent;
-import org.eclipse.kura.camel.router.AbstractCamelRouter;
+import org.eclipse.kura.camel.runner.BeforeStart;
+import org.eclipse.kura.camel.runner.CamelRunner;
+import org.eclipse.kura.camel.runner.CamelRunner.Builder;
 import org.eclipse.kura.configuration.ConfigurableComponent;
+import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Example for using AMQP
  */
-public class ExampleRouterJava extends AbstractCamelRouter implements ConfigurableComponent {
+public class ExampleRouterJava implements ConfigurableComponent {
 
     private static final Logger logger = LoggerFactory.getLogger(ExampleRouterJava.class);
 
     private String brokerUrl;
-    private String topic;
 
-    public ExampleRouterJava() {
-        // load defaults
-        loadProperties(null);
-    }
+    private CamelRunner camelRunner;
 
-    public void start(final Map<String, Object> properties) throws Exception {
+    public void start(final BundleContext context, final Map<String, Object> properties) throws Exception {
         logger.info("Starting");
-        
-        loadProperties(properties);
-        super.start();
+
+        // apply configuration
+
+        applyMajorChange(properties);
+
+        // start router
+
+        this.camelRunner = createRunner();
+        setRoutesFromProperties(properties);
+        this.camelRunner.start();
     }
 
     public void updated(final Map<String, Object> properties) throws Exception {
         logger.info("Updating");
-        
-        stop();
-        loadProperties(properties);
-        start();
+
+        // apply configuraton
+
+        if (applyMajorChange(properties)) {
+
+            // require restart
+
+            this.camelRunner.stop();
+            this.camelRunner = createRunner();
+            setRoutesFromProperties(properties);
+            this.camelRunner.start();
+
+        } else {
+
+            // apply on the fly
+
+            setRoutesFromProperties(properties);
+
+        }
     }
 
-    private void loadProperties(final Map<String, Object> properties) {
-        this.brokerUrl = asString(properties, "brokerUrl", "amqp://localhost:5672");
-        this.topic = asString(properties, "topic", "test");
+    private CamelRunner createRunner() {
 
-        logger.info("Properties loaded - brokerUrl: {}, topic: {}", this.brokerUrl, this.topic);
+        // create a new router builder
+
+        final Builder builder = new CamelRunner.Builder();
+
+        // register AMQP
+
+        builder.addBeforeStart(new BeforeStart() {
+            @Override
+            public void beforeStart(CamelContext camelContext) throws Exception {
+                registerAmqp(camelContext);
+            }
+        });
+
+        // build runner
+
+        return builder.build();
     }
 
-    @Override
-    protected void registerFeatures(final CamelContext camelContext) {
+    private void registerAmqp(CamelContext camelContext) {
+
+        // Register AMQP component current broker URL
+
         final AMQPComponent amqp = AMQPComponent.amqpComponent(this.brokerUrl);
         camelContext.addComponent("amqp", amqp);
     }
 
-    @Override
-    public void configure() {
-        logger.info("Configure - brokerUrl: {}, topic: {}", this.brokerUrl, this.topic);
+    /**
+     * Apply major change configuration
+     * <p>
+     * This method selectively applies configuration which does request a Camel router
+     * restart.
+     * </p>
+     * 
+     * @param properties
+     *            The properties to apply
+     * @return {@code true} if a router restart is necessary, {@code false} otherwise
+     */
+    private boolean applyMajorChange(Map<String, Object> properties) {
+        boolean changed = false;
 
-        from("timer:test").setBody().simple("foo").to("amqp:topic://" + this.topic);
-    }
-
-    private static String asString(final Map<String, Object> properties, final String key, final String defaultValue) {
-        final Object value = properties != null ? properties.get(key) : null;
-
-        if (value instanceof String) {
-            return (String) value;
+        final String newBrokerUrl = asString(properties, "brokerUrl", "amqp://localhost:5432");
+        if (this.brokerUrl == null || !this.brokerUrl.equals(newBrokerUrl)) {
+            this.brokerUrl = newBrokerUrl;
+            changed = true;
         }
 
-        return defaultValue;
+        return changed;
     }
 
+    /**
+     * Apply Camel routes from configuration properties
+     * 
+     * @param properties
+     *            the properties to apply
+     * @throws Exception
+     *             if anything goes wrong
+     */
+    private void setRoutesFromProperties(Map<String, Object> properties) throws Exception {
+        this.camelRunner.setRoutes(buildRoutes(properties));
+    }
+
+    /**
+     * Create a new instance of {@link RouteBuilder} which reflects the current state of the component
+     * <p>
+     * Please not that each instance of {@link RouteBuilder} will only build the routes once. So re-applying
+     * the same instance of {@link RouteBuilder} will not trigger a second call to {@link RouteBuilder#configure()}
+     * </p>
+     * 
+     * @param properties
+     *            the configuration properties to build the routes from
+     * @return the routes builder instance
+     */
+    private RouteBuilder buildRoutes(final Map<String, Object> properties) {
+        final String topic = asString(properties, "topic", "test");
+
+        logger.info("Properties loaded - topic: {}", topic);
+
+        // always return a new instance of RouteBuilder
+        
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("timer:test").setBody().simple("foo").to("amqp:topic://" + topic);
+            }
+        };
+    }
 }
